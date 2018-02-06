25566c9d6043eef41642e2084156920ddc4fdf11
nhat.nguyenhong <hongnhatbkhn@gmail.com>
2018-02-06 18:59:54 +0700

    * fix include paths

--- include/clover.h
+++ include/clover.h
@@ -1,11 +1,11 @@
 #ifndef __CLOVER_H__
 #define __CLOVER_H__
 
-#include <Logger.h>
-#include <Sink.h>
-#include <SinkTopic.h>
-#include <VSOutputSink.h>
+#include "Logger.h"
+#include "Sink.h"
+#include "SinkTopic.h"
+#include "VSOutputSink.h"
 
-#include <Tracer.h>
+#include "Tracer.h"
 
 #endif // __CLOVER_H__
