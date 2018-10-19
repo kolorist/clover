a9b149b0b90449ff97ad61d8162d5b83c7b2ba50
nhat.nguyenhong <hongnhatbkhn@gmail.com>
2018-10-19 17:58:05 +0700

    thread_local sink topic

--- src/VSOutputSink.cpp
+++ src/VSOutputSink.cpp
@@ -4,52 +4,55 @@
 #include <stdio.h>
 
 namespace clover {
-	VSOutputSink								g_VSOutputSink;
 
-	static floral::mutex						s_VSSinkLock;
+VSOutputSink									g_VSOutputSink;
+thread_local const_cstr							s_SinkTopics[MAX_SINK_TOPIC_DEPTH];
+thread_local u32								s_CurrentTopicIdx = 0;
 
-	void VSOutputSinkDrainer::DrainLog(LogLevel logLevel, const_cstr logStr)
-	{
-		if (logLevel <= g_VSOutputSink.pm_LogLevel) {
-			floral::lock_guard g(s_VSSinkLock);
-			c8 buffer[2048];
-			if (g_VSOutputSink.pm_CurrentTopicIdx > 0) {
-				sprintf(buffer, "[%s] [/%s] [%s] %s",
-						g_VSOutputSink.pm_Name,
-						g_VSOutputSink.pm_SinkTopics[g_VSOutputSink.pm_CurrentTopicIdx - 1],
-						LogLevelStr[(s32)logLevel],
-						logStr);
-			} else {
-				sprintf(buffer, "[%s] [/] [%s] %s",
-						g_VSOutputSink.pm_Name,
-						LogLevelStr[(s32)logLevel],
-						logStr);
-			}
-			OutputDebugStringA(buffer);
-			OutputDebugStringA("\n");
+static floral::mutex							s_VSSinkLock;
+
+void VSOutputSinkDrainer::DrainLog(LogLevel logLevel, const_cstr logStr)
+{
+	if (logLevel <= g_VSOutputSink.pm_LogLevel) {
+		floral::lock_guard g(s_VSSinkLock);
+		c8 buffer[2048];
+		if (s_CurrentTopicIdx > 0) {
+			sprintf(buffer, "[%s] [/%s] [%s] %s",
+					g_VSOutputSink.pm_Name,
+					s_SinkTopics[s_CurrentTopicIdx - 1],
+					LogLevelStr[(s32)logLevel],
+					logStr);
+		} else {
+			sprintf(buffer, "[%s] [/] [%s] %s",
+					g_VSOutputSink.pm_Name,
+					LogLevelStr[(s32)logLevel],
+					logStr);
 		}
+		OutputDebugStringA(buffer);
+		OutputDebugStringA("\n");
 	}
+}
 
-	void VSOutputSinkDrainer::PushTopic(const_cstr topicName)
-	{
-		floral::lock_guard g(s_VSSinkLock);
-		g_VSOutputSink.pm_SinkTopics[g_VSOutputSink.pm_CurrentTopicIdx] = topicName;
-		g_VSOutputSink.pm_CurrentTopicIdx++;
-	}
+void VSOutputSinkDrainer::PushTopic(const_cstr topicName)
+{
+	//floral::lock_guard g(s_VSSinkLock);
+	s_SinkTopics[s_CurrentTopicIdx] = topicName;
+	s_CurrentTopicIdx++;
+}
 
-	void VSOutputSinkDrainer::PopTopic()
-	{
-		floral::lock_guard g(s_VSSinkLock);
-		g_VSOutputSink.pm_SinkTopics[g_VSOutputSink.pm_CurrentTopicIdx] = nullptr;
-		g_VSOutputSink.pm_CurrentTopicIdx--;
-	}
+void VSOutputSinkDrainer::PopTopic()
+{
+	//floral::lock_guard g(s_VSSinkLock);
+	s_SinkTopics[s_CurrentTopicIdx] = nullptr;
+	s_CurrentTopicIdx--;
+}
 
-	////////////////////////////////////////////
-	void InitializeVSOutput(const_cstr name, LogLevel logLevel)
-	{
-		strcpy(g_VSOutputSink.pm_Name, name);
-		g_VSOutputSink.pm_LogLevel = logLevel;
-		g_VSOutputSink.pm_CurrentTopicIdx = 0;
-	}
-	////////////////////////////////////////////
+////////////////////////////////////////////
+void InitializeVSOutput(const_cstr name, LogLevel logLevel)
+{
+	strcpy(g_VSOutputSink.pm_Name, name);
+	g_VSOutputSink.pm_LogLevel = logLevel;
+}
+////////////////////////////////////////////
+
 }
